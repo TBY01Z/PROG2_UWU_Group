5df2c331f73a49346689a85fd0c3909b203ab6d7
Mark <mark.fischer0404@gmail.com>
2022-07-03 19:52:50 +0200

    server und client sachen sind nun eingebaut (sogar mit mosemanns geliebten threads) :)

---
+++ src/main/java/com/prog2/uwugroup/Player.java
@@ -0,0 +1,13 @@
+package com.prog2.uwugroup;
+
+public class Player {
+
+    public int id;
+    public String name;
+
+    public Player(int id, String name){
+        this.id = id;
+        this.name= name;
+    }
+
+}
---
+++ src/main/java/com/prog2/uwugroup/packets/AddPlayerPacket.java
@@ -0,0 +1,12 @@
+package com.prog2.uwugroup.packets;
+
+import java.io.Serializable;
+
+public class AddPlayerPacket implements Serializable {
+
+    private static final long serialVersionUID = 1l;
+
+    public int id;
+    public String name;
+
+}
