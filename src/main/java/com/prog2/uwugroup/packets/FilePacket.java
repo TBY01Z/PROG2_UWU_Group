35605fe13c5d507732c9eed23b4b8511be6fa243
Niclas <rieckersniclas@gmail.com>
2022-07-05 14:35:32 +0200

    sinnvoller Kommentar

--- src/main/java/com/prog2/uwugroup/packets/FilePacket.java
+++ src/main/java/com/prog2/uwugroup/packets/FilePacket.java
@@ -1,8 +1,26 @@
 package com.prog2.uwugroup.packets;
 
+import java.io.File;
+import java.io.IOException;
 import java.io.Serializable;
 
 public class FilePacket implements Serializable {
     private static final long serialVersionUID = 1l;
     public int id;
+    private File file;
+    private String path;
+
+    public FilePacket(File file) {
+        this.file = file;
+        this.path = file.getPath();
+    }
+    public void download(){
+        try {
+            file.createNewFile();
+            file.getPath();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
