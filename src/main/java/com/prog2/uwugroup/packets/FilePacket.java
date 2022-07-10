43201c04af0a3c635a46548257cdcc3ed17ae985
Niclas <rieckersniclas@gmail.com>
2022-07-10 21:26:18 +0200

    sinnvoller Kommentar

--- src/main/java/com/prog2/uwugroup/packets/FilePacket.java
+++ src/main/java/com/prog2/uwugroup/packets/FilePacket.java
@@ -1,9 +1,7 @@
 package com.prog2.uwugroup.packets;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.Serializable;
+
+import java.io.*;
 import java.util.Scanner;
 
 // File recived -> chose accept ->
@@ -13,34 +11,44 @@ public class FilePacket implements Serializable {
     public int id;
     private String path = System.getProperty("user.dir") + "\\model.txt";
     private String inputString = "";
+    private File file;
     public FilePacket(String path){
         this.setPath(path);
-        File file = new File(this.getPath());
+        file = new File(this.getPath());
+        if(!file.exists()){
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
         Scanner scanner;
         try {
             scanner = new Scanner(file);
             while(scanner.hasNextLine()){
                 inputString = inputString + scanner.nextLine();
             }
-        } catch (FileNotFoundException ex) {
-            ex.printStackTrace();
-            File file1=new File(path);
-            try {
-                file1.createNewFile();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
         }
     }
 
-    public String[] getInputString() {
+
+    private String[] getInputString() {
         return inputString.split("\n\t\r");
     }
 
     public void setInputString(String inputString) {
         this.inputString = inputString;
     }
-
+    public void download(String path){
+        try {
+            FileOutputStream outputStream = new FileOutputStream(path);
+            outputStream.write(inputString.getBytes());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
     public String getPath() {
         return path;
     }
