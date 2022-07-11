1ffb0f6947f2699125fc9d65e1c5d8bd8f5dec5e
Niclas <rieckersniclas@gmail.com>
2022-07-11 09:47:22 +0200

    sinnvoller Kommentar

--- src/main/java/com/prog2/uwugroup/packets/FilePacket.java
+++ src/main/java/com/prog2/uwugroup/packets/FilePacket.java
@@ -1,26 +1,59 @@
 package com.prog2.uwugroup.packets;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.Serializable;
 
+import java.io.*;
+import java.util.Scanner;
+
+// File received -> chose accept ->
+// File send -> chose File -> accept
 public class FilePacket implements Serializable {
     private static final long serialVersionUID = 1l;
     public int id;
-    private File file;
     private String path;
-
-    public FilePacket(File file) {
-        this.file = file;
-        this.path = file.getPath();
-    }
-    public void download(){
+    private String inputString = "";
+    private File file;
+    public FilePacket(String path){
+        this.setPath(path);
+        file = new File(this.getPath());
+        if(!file.exists()){
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        Scanner scanner;
         try {
-            file.createNewFile();
-            file.getPath();
+            scanner = new Scanner(file);
+            while(scanner.hasNextLine()){
+                inputString = inputString + scanner.nextLine();
+            }
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
+    }
 
-        } catch (IOException e) {
+
+    private String[] getInputString() {
+        return inputString.split("\n\t\r");
+    }
+
+    public void setInputString(String inputString) {
+        this.inputString = inputString;
+    }
+    public void download(String path){
+        try {
+            FileOutputStream outputStream = new FileOutputStream(path);
+            outputStream.write(inputString.getBytes());
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
     }
 }
