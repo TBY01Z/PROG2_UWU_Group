46790105ed598f9fd9b2655495121b09808b9403
Niclas <rieckersniclas@gmail.com>
2022-07-11 10:19:54 +0200

    sinnvoller Kommentar

---
+++ src/main/java/com/prog2/uwugroup/packets/ChatMessages.java
@@ -0,0 +1,23 @@
+package com.prog2.uwugroup.packets;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+public class ChatMessages {
+    private final ObservableList<MessagePacket> chat = FXCollections.emptyObservableList();
+
+    public ChatMessages() {
+    }
+    //
+    public boolean add(MessagePacket message){
+        if(!chat.contains(message)){
+            chat.add(message);
+            return true;
+        }
+        return false;
+    }
+    //
+    public ObservableList<MessagePacket> chat() {
+        return chat;
+    }
+}
