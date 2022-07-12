1c1c6e0f92e120824bd153136c0d712e478749d1
Niclas <rieckersniclas@gmail.com>
2022-07-12 16:51:25 +0200

    sinnvoller Kommentar

--- src/main/java/com/prog2/uwugroup/packets/ChatMessages.java
+++ src/main/java/com/prog2/uwugroup/packets/ChatMessages.java
@@ -3,7 +3,9 @@ package com.prog2.uwugroup.packets;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
-public class ChatMessages {
+import java.io.Serializable;
+
+public class ChatMessages implements Serializable {
     private final ObservableList<MessagePacket> chat = FXCollections.observableArrayList();
     public ChatMessages() {}
     public boolean add(MessagePacket message){
