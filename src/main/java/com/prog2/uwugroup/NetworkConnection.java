5fb1ef0233d5e4cbe03c48bf0887ef8d30d58d27
Niclas <rieckersniclas@gmail.com>
2022-07-14 20:47:18 +0200

    sinnvoller Kommentar

--- src/main/java/com/prog2/uwugroup/NetworkConnection.java
+++ src/main/java/com/prog2/uwugroup/NetworkConnection.java
@@ -17,7 +17,7 @@ public abstract class NetworkConnection {
     }
 
     public void startConnection() throws Exception{
-        connectionThread.run();
+        connectionThread.start();
     }
 
     public void send(Serializable data) throws Exception{
