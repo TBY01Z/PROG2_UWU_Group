e581388a1f4aaa317989b879d2f16a46b8de160f
Mark <mark.fischer0404@gmail.com>
2022-07-14 20:01:04 +0200

    ahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh

--- src/main/java/com/prog2/uwugroup/StartStage.java
+++ src/main/java/com/prog2/uwugroup/StartStage.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 
 public class StartStage extends Application {
     private boolean isServer = false;   //hier wird bestimmt ob man server oder client ist
+    //TODO: am besten im main menu nen button machen, mit dem man hosten oder joinen kann
 
     @FXML
     private TextArea messages;
