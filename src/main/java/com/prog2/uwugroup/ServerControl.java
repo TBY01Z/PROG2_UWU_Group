0f6116a9fe02b9f97abe3f35ff1b5e3c0d0c1d3e
mark <mark.fischer0404@gmail.com>
2022-07-16 17:39:17 +0200

    für niclas

--- src/main/java/com/prog2/uwugroup/ServerControl.java
+++ src/main/java/com/prog2/uwugroup/ServerControl.java
@@ -1,12 +1,39 @@
 package com.prog2.uwugroup;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
 import java.net.URL;
+import java.net.UnknownHostException;
 import java.util.ResourceBundle;
 
+import static com.prog2.uwugroup.ClientHandler.clientHandlers;
+
 public class ServerControl implements Initializable {
+
+    @FXML
+    private TableView userTable = new TableView();
+    @FXML
+    private Label ipLabel = new Label();
+
+    private ObservableList portArray = FXCollections.observableArrayList();//{8080, 8484, 2564, 2556};
+
+    private ObservableList<ObservableList> choiceData = FXCollections.observableArrayList(portArray);
+    private ObservableList<ClientHandler> clientsList = FXCollections.observableArrayList(clientHandlers);
+    @FXML
+    private ChoiceBox portChoiceBox = new ChoiceBox(choiceData.get(0));
+
+    private static Integer selectedPort;
 
     /**
      * Called to initialize a controller after its root element has been
@@ -19,17 +46,45 @@ public class ServerControl implements Initializable {
      */
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        userTable.setItems(clientsList);
+        portArray.addAll(8080,8484,2564,2556);
+        try {
+            ipLabel.setText(InetAddress.getLocalHost().getHostAddress());
+            System.out.println(InetAddress.getLocalHost().getHostAddress());    //TODO: remove later!
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void kickPlayer(ActionEvent event) {
+    public void kickClient(ActionEvent event) {
+        
+    }
+
+    public void startServer() throws IOException {
+        NewServer server = new NewServer(new ServerSocket(selectedPort));
+        server.startServer();
     }
 
     public void closeServer(ActionEvent event) {
+        try {
+            NewServer server = new NewServer(new ServerSocket(selectedPort));
+            server.closeServer();
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
 
+    public void portSelect(ActionEvent event){
+        selectedPort = Integer.valueOf((String) portChoiceBox.getValue());
     }
 
     public void aboutInfo(ActionEvent event) {
-
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("UWU Gruppe");
+        alert.setHeaderText("About");
+        String s = "Dies ist eine Chat-Anwendung, geschrieben von Niclas Rieckers und Mark Fischer." +
+                "Diese Anwendung wurde im Rahmen des Moduls PROG2 entwickelt";//TODO: change about
+        alert.setContentText(s);
+        alert.show();
     }
 }
